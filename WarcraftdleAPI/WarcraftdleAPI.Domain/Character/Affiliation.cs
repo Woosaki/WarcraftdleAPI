8eb4aa9d64834258ab2bb3de144f84b5f9b781c5
Woosaki <kamil.szczukowski@interia.pl>
2023-12-15 01:08:39 +0100

    Add JsonIgnore adnotations

--- WarcraftdleAPI/WarcraftdleAPI.Domain/Character/Affiliation.cs
+++ WarcraftdleAPI/WarcraftdleAPI.Domain/Character/Affiliation.cs
@@ -1,8 +1,11 @@
-﻿namespace WarcraftdleAPI.Domain.Character;
+﻿using System.Text.Json.Serialization;
+
+namespace WarcraftdleAPI.Domain.Character;
 
 public class Affiliation
 {
 	public int Id { get; set; }
 	public string Name { get; set; } = null!;
+	[JsonIgnore]
 	public ICollection<WowCharacter> WowCharacters { get; set; } = null!;
 }
--- WarcraftdleAPI/WarcraftdleAPI.Domain/Character/Expansion.cs
+++ WarcraftdleAPI/WarcraftdleAPI.Domain/Character/Expansion.cs
@@ -1,9 +1,12 @@
-﻿namespace WarcraftdleAPI.Domain.Character;
+﻿using System.Text.Json.Serialization;
+
+namespace WarcraftdleAPI.Domain.Character;
 
 public class Expansion
 {
 	public int Id { get; set; }
 	public string Name { get; set; } = null!;
 	public string? Abbreviation { get; set; }
+	[JsonIgnore]
 	public ICollection<WowCharacter> WowCharacters { get; set; } = null!;
 }
