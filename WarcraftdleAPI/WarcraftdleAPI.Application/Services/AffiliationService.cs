98e4781ee277c9d3cfd17a5d3c0e8c7d4c8c0d07
Woosaki <kamil.szczukowski@interia.pl>
2024-01-04 20:33:54 +0100

    Add get methods for affiliations

---
+++ WarcraftdleAPI/WarcraftdleAPI.Application/Services/AffiliationService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarcraftdleAPI.Domain.WowCharacter;
+using WarcraftdleAPI.Infrastructure;
+
+namespace WarcraftdleAPI.Application.Services;
+
+public class AffiliationService(WarcraftdleDbContext dbContext)
+{
+	public async Task<IEnumerable<Affiliation>> GetAsync()
+	{
+		var affiliations = await dbContext.Affiliation.ToListAsync();
+
+		return affiliations;
+	}
+
+	public async Task<Affiliation> GetByIdAsync(int id)
+	{
+		var affiliation = await dbContext.Affiliation.FirstOrDefaultAsync(x => x.Id == id);
+
+		return affiliation;
+	}
+}
---
+++ WarcraftdleAPI/WarcraftdleAPI/Controllers/AffiliationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using WarcraftdleAPI.Domain.WowCharacter;
+
+namespace WarcraftdleAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AffiliationController : ControllerBase
+{
+	[HttpGet]
+	public async Task<IEnumerable<Affiliation>> Get()
+	{
+
+	}
+}
