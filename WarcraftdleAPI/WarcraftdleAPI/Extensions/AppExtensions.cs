84a84d77e50a95a271ef77cfd03b996b86e09432
Woosaki <kamil.szczukowski@interia.pl>
2024-01-09 22:06:35 +0100

    Cleanup Program.cs

---
+++ WarcraftdleAPI/WarcraftdleAPI/Extensions/AppExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarcraftdleAPI.Infrastructure;
+using WarcraftdleAPI.Middlewares;
+
+namespace WarcraftdleAPI.Extensions;
+
+public static class AppExtensions
+{
+	public static void ConfigureSwagger(this WebApplication app)
+	{
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+	}
+
+	public static void ConfigureMiddleware(this WebApplication app)
+	{
+		app.UseMiddleware<ExceptionHandlerMiddleware>();
+	}
+
+	public static void MigrateDatabase(this WebApplication app)
+	{
+		using var scope = app.Services.CreateScope();
+		var db = scope.ServiceProvider.GetRequiredService<WarcraftdleDbContext>();
+
+		if (db.Database.GetPendingMigrations().Any())
+		{
+			db.Database.Migrate();
+		}
+	}
+}
--- WarcraftdleAPI/WarcraftdleAPI/Program.cs
+++ WarcraftdleAPI/WarcraftdleAPI/Program.cs
@@ -1,74 +1,15 @@
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Microsoft.EntityFrameworkCore;
-using WarcraftdleAPI.Application.Dtos.Affiliations;
-using WarcraftdleAPI.Application.Dtos.WowCharacters;
-using WarcraftdleAPI.Application.Dtos.Zones;
-using WarcraftdleAPI.Application.Services;
-using WarcraftdleAPI.Application.Validators.Affiliations;
-using WarcraftdleAPI.Application.Validators.WowCharacters;
-using WarcraftdleAPI.Application.Validators.Zones;
-using WarcraftdleAPI.Infrastructure;
-using WarcraftdleAPI.Middlewares;
+using WarcraftdleAPI.Extensions;
 
 namespace WarcraftdleAPI;
 
 public static class Program
 {
-	private static void ConfigureDatabase(this WebApplicationBuilder builder)
-	{
-		var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
-		builder.Services.AddDbContext<WarcraftdleDbContext>(
-			options => options.UseNpgsql(connectionString));
-	}
-
-	private static void ConfigureServices(this WebApplicationBuilder builder)
-	{
-		builder.Services.AddScoped<AffiliationService>();
-		builder.Services.AddScoped<ZoneService>();
-		builder.Services.AddScoped<WowCharacterService>();
-
-		builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
-		builder.Services.AddTransient<IValidator<AddWowCharacterRequest>, AddWowCharacterRequestValidator>();
-		builder.Services.AddTransient<IValidator<AddZoneRequest>, AddZoneRequestValidator>();
-		builder.Services.AddTransient<IValidator<AddAffiliationRequest>, AddAffiliationRequestValidator>();
-		builder.Services.AddTransient<IValidator<AddMultipleAffiliationRequest>, AddMultipleAffiliationRequestValidator>();
-		builder.Services.AddTransient<IValidator<AddMultipleZoneRequest>, AddMultipleZoneRequestValidator>();
-
-		builder.Services.AddControllers();
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen();
-	}
-
-	private static void ConfigureSwagger(this WebApplication app)
-	{
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
-	}
-
-	private static void ConfigureMiddleware(this WebApplication app)
-	{
-		app.UseMiddleware<ExceptionHandlerMiddleware>();
-	}
-
-	private static void MigrateDatabase(this WebApplication app)
-	{
-		using var scope = app.Services.CreateScope();
-		var db = scope.ServiceProvider.GetRequiredService<WarcraftdleDbContext>();
-
-		if (db.Database.GetPendingMigrations().Any())
-		{
-			db.Database.Migrate();
-		}
-	}
-
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
+
 		builder.ConfigureDatabase();
+		builder.ConfigureFluentValidation();
 		builder.ConfigureServices();
 
 		var app = builder.Build();
